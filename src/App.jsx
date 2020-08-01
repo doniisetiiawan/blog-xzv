3dd8728a44428717213661ed53e3346413d6ba51
Doni Setiawan <donisetiawan.050@gmail.com>
2020-08-01 19:13:19 +0700

    Solving common problems with Hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,43 @@
 import React, { useState } from 'react';
 
 function App() {
+  const [enableFirstName, setEnableFirstName] = useState(
+    false,
+  );
   const [name, setName] = useState('');
+  const [lastName, setLastName] = useState('');
 
   function handleChange(evt) {
     setName(evt.target.value);
   }
 
+  function handleLastNameChange(evt) {
+    setLastName(evt.target.value);
+  }
+
+  function handleEnableChange() {
+    setEnableFirstName(!enableFirstName);
+  }
+
   return (
     <div>
-      <h1>My name is: {name}</h1>
+      <h1>
+        My name is: {enableFirstName ? name : ''} {lastName}
+      </h1>
+      <input
+        type="checkbox"
+        value={enableFirstName}
+        onChange={handleEnableChange}
+      />
       <input
         type="text"
         value={name}
         onChange={handleChange}
+      />
+      <input
+        type="text"
+        value={lastName}
+        onChange={handleLastNameChange}
       />
     </div>
   );
